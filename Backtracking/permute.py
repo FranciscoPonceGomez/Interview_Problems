c6ef4a5f35a948d35a7ea15a5746597dfb561114
Francisco Ponce Gomez <pakopongo@gmail.com>
2018-03-22 09:59:37 -0700

    Update permute.py

--- Backtracking/permute.py
+++ Backtracking/permute.py
@@ -1,9 +1,10 @@
 def permute(A,index):
-	if index == len(A)
-	for j in range (index,len(A)):
-		A[index],A[j] = A[j],A[index]
-		permute(A,index+1)
-		A[index],A[j] = A[j],A[index]
+    if index == len(A):
+        print(A)
+    for j in range (index,len(A)):
+        A[index],A[j] = A[j],A[index]
+        permute(A,index+1)
+        A[index],A[j] = A[j],A[index]
 
 A = ['A','B','C','D']
 permute(A,0)
